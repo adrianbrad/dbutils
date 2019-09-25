d32f5d7c03742f283c49f8f8c770126487069a71
brad <me@adrianbrad.com>
2019-09-25 11:50:22 +0300

    improved attempt connect method to report errors

--- attemptconn.go
+++ attemptconn.go
@@ -2,12 +2,19 @@ package dbutils
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 )
 
+type errors []error
+
+func (e errors) Error() string {
+	return ""
+}
+
 // tries- default 0, runs 1000 attempts
 // wait - default 0, wait 1 second
-func AttemptConnectPostgres(ds DataSource, tries int, wait time.Duration) (*sql.DB, error) {
+func AttemptConnect(driver string, ds DataSource, tries int, wait time.Duration) (*sql.DB, bool, error) {
 	var db *sql.DB
 	var err error
 
@@ -18,22 +25,27 @@ func AttemptConnectPostgres(ds DataSource, tries int, wait time.Duration) (*sql.
 		tries = 1000
 	}
 
+	var errs errors
+	var ok bool
 	for i := 1; i <= tries; i++ {
-		db, err = sql.Open("postgres", ds.String())
+		db, err = sql.Open(driver, ds.String())
 		if err != nil {
+			errs = append(errs, fmt.Errorf("dbutils.AttemptConnect: attempt number: %d error :%w", i, err))
 			time.Sleep(time.Second * wait)
 			continue
 		}
+		ok = true
 		break
 	}
-	if err != nil {
-		return nil, err
+	if !ok {
+		return nil, false, errs
 	}
 
 	err = db.Ping()
 	if err != nil {
 		db.Close()
-		return nil, err
+		return nil, false, fmt.Errorf("dbutils.AttemptConnect: while pinging database: %w", err)
 	}
-	return db, nil
+
+	return db, true, errs
 }
